20e4ca221bef08ad6d9f48f3f1f76ba04ce6876a
jstd01t <commontasksfor@gmail.com>
2022-03-15 17:44:43 +0200

    fix problem with command

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -2,21 +2,16 @@
 
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
 use Symfony\Contracts\Cache\CacheInterface;
 
-#[AsCommand(
-    name: 'app:step:info',
-    description: 'The command displays the Git tag name attached to the current Git commit',
-)]
 class StepInfoCommand extends Command
 {
+    protected static $defaultName = 'app:step:info';
+
     private $cache;
 
     public function __construct(CacheInterface $cache)
