14be7cd0c624df54572b4bf5c64d799d700a4d6e
jstd01t <commontasksfor@gmail.com>
2022-03-15 13:26:53 +0200

    add ESI to avoid SQL requests for showing conferences on the website's pages

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -41,6 +41,14 @@ class ConferenceController extends AbstractController
         return $response;
     }
 
+    #[Route('/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        return new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
     #[Route('conference/{slug}', name: 'conference')]
     public function show(
         Request              $request,
