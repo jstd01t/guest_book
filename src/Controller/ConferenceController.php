32d301062708be3a08ebbfb0b6ccd071a6e20f67
jstd01t <commontasksfor@gmail.com>
2022-03-15 13:34:07 +0200

    remove TwigEventSubscriber as we don't need it anymore

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -44,9 +44,12 @@ class ConferenceController extends AbstractController
     #[Route('/conference_header', name: 'conference_header')]
     public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response($this->twig->render('conference/header.html.twig', [
+        $response =  new Response($this->twig->render('conference/header.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
     }
 
     #[Route('conference/{slug}', name: 'conference')]
