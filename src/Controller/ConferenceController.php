f5a41288a7f6b42102c19e306cedc64bb90a5a13
jstd01t <commontasksfor@gmail.com>
2022-03-17 15:16:35 +0200

    add Slack messaging system

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -18,6 +18,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ConferenceController extends AbstractController
 {
@@ -89,8 +90,8 @@ class ConferenceController extends AbstractController
                 'permalink' => $request->getUri(),
             ];
 
-            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
-
+            $reviewUrl = $this->generateUrl('review_comment', ['id' => $comment->getId(), UrlGeneratorInterface::ABSOLUTE_URL]);
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $reviewUrl, $context));
             $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
